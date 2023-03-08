fe805d62edd7b17b18d2707a3d71e6577acd8c79
santhosh3 <santhoshchinna109@gmail.com>
2023-03-08 10:10:21 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const serverless = require('serverless-http');
 
 const Routes = require("./src/router/routes")
 const app = express();
